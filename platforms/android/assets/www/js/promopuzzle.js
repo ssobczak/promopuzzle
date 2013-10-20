ce6c791106f984489709a9d8760ad8931dd5aa4a
bartoszlenar <bartoszlenar@gmail.com>
2013-10-20 02:22:49 +0200

    menu + base images

--- platforms/android/assets/www/js/promopuzzle.js
+++ platforms/android/assets/www/js/promopuzzle.js
@@ -1,7 +1,7 @@
-function promoPuzzle(userId) {
+function PromoPuzzle(serverUrl, userId) {
 	var self = this;
 
-	self.ServerUrl = ko.observable('http://localhost:3000');
+	self.ServerUrl = ko.observable(serverUrl);
 	self.UserId = ko.observable(userId);
 
     self.Url = function(url, params) {
@@ -14,15 +14,57 @@ function promoPuzzle(userId) {
     	return result;
     };
 
-	self.FetchPuzzles = function() {
+	self.ShowPuzzles = function() {
 		$.ajax({
 		    type: "GET",
 		    url: self.Url('api/images.json', {user_id: self.UserId()})
 		}).done(function(data) {
 		   debugger;
-		});
+		   	
 
+		});
 	};
 
+
+	self.ToStage = function(model, name) {
+		var stageHtml = $('.purgatory').find('.' + name).html();
+		$('.app').html(stageHtml);
+
+		ko.applyBindings(model, $('.app')[0]);
+	};
+
+
+
+
+};
+
+
+function MenuViewModel() {
+	var self = this;
+
+	self.BrandLogoClickedEvent = undefined;
+
+	self.BrandLogoClicked = function() {
+		console.log('BrandLogoClicked');
+		if (self.BrandLogoClickedEvent) {
+			self.BrandLogoClickedEvent();
+		}
+	};
+}
+
+
+function ImagesViewModel(){
+	var self = this;
+
+	self.Init = function(imagesJson) {
+
+	};
+};
+
+
+function strona() {
+	var self = this;
+
+	self.Text = ko.observable('TEST strony');
 };
 
--- rails/public/js/promopuzzle.js
+++ rails/public/js/promopuzzle.js
@@ -14,45 +14,55 @@ function PromoPuzzle(serverUrl, userId) {
     	return result;
     };
 
+    self.TotalPuzzles = ko.observable(0);
+
 	self.ShowPuzzles = function() {
 		$.ajax({
 		    type: "GET",
 		    url: self.Url('api/images.json', {user_id: self.UserId()})
 		}).done(function(data) {
-		   debugger;
-		   	var viewModel = new imagesViewModel();
-		   	viewModel
-		   	self.ToStage(viewModel, )
+			var temp = [];
+			var totalPuzzles = 0;
+			
+			for(var i=0; i<data.length; ++i){
+				totalPuzzles += data[i].parts.length;
+				temp.push(ko.viewmodel.fromModel(data[i], self.ImagesMappingOptions));
+			}
 
+			self.TotalPuzzles(totalPuzzles);
+			self.Puzzles(temp);
+
+			self.ToStage('images');
 		});
 	};
 
 
-	self.ToStage = function(model, name) {
+	self.Puzzles = ko.observableArray();
+
+	self.ToStage = function(name, model) {
+		debugger;
 		var stageHtml = $('.purgatory').find('.' + name).html();
 		$('.app').html(stageHtml);
-
-		ko.applyBindings(model, $('.app')[0]);
+		if (!model) {
+			model = self;
+		}
+		ko.cleanNode($('.app')[0]);
+		ko.applyBindings(self, $('.app')[0]);
 	};
 
 
-
-
-};
-
-
-function ImagesViewModel(){
-	var self = this;
-
-	self.Init = function(imagesJson) {
-		
+	self.BrandLogoClicked = function() {
+		self.ShowPuzzles();
 	};
+
+	self.ImagesMappingOptions = { 
+	    extend: {
+	        "{root}": function(image){
+	            debugger;
+	        }
+	    }
+	};
+
 };
 
-
-function strona() {
-	var self = this;
-
-	self.Text = ko.observable('TEST strony');
-};
 
