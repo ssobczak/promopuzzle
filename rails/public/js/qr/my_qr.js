da5795590a38ad6a28352b1a6fe8fbdddf775c06
Szymon Sobczak <szymon.sobczak@gmail.com>
2013-10-20 05:44:45 +0200

    generate QR

---
+++ rails/public/js/qr/my_qr.js
@@ -0,0 +1,14 @@
+function create_qrcode(text, typeNumber, errorCorrectLevel, table) {
+
+	var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
+	qr.addData(text);
+	qr.make();
+
+//	return qr.createTableTag();
+	return qr.createImgTag();
+};
+
+var update_qrcode = function(to_encode) {
+	var text = String(to_encode).replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
+	document.getElementById('qr_image').innerHTML = create_qrcode(text);
+};
